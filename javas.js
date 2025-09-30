c6354a79d7f1be0a5ec8b1da33e5a686e935abc6
mot546 <jellopancake213@gmail.com>
2025-09-30 20:27:37 +0800

    Add reset button and Fix resizing button

--- javas.js
+++ javas.js
@@ -2,24 +2,46 @@
 
 const container = document.querySelector(".container");
 const contSize = 400;
-
+const submitButton = document.querySelector("#submit");
+let squares = 16;
 let square;
-let squares = 30;
-let widthHeight = contSize / squares;
 
-for(let i=0;squares*squares > i; i++){
-    square = document.createElement("div");
-    square.className = "square";
-    square.style.width = `${widthHeight}px`;
-    square.style.height = `${widthHeight}px`;
-    square.classList.add(`s${i}`);
-    container.appendChild(square);
-}
- 
-for(i = 0; squares*squares > i; i++){
-    let thisOne = document.querySelector(`.s${i}`);
-    thisOne.addEventListener("mouseover", function(){
-        thisOne.classList.add("over");
-    });
-}
+function divBox(){
+    let widthHeight = contSize / squares;
     
+    for(let i=0;squares*squares > i; i++){
+        square = document.createElement("div");
+        square.className = "square";
+        square.style.width = `${widthHeight}px`;
+        square.style.height = `${widthHeight}px`;
+        square.classList.add(`s${i}`);
+        container.appendChild(square);
+        squares>30? square.style.border = "none": square.style.border = ".5px solid black";
+    }
+    
+    //hover effect
+    for(let i = 0; squares*squares > i; i++){
+        let thisOne = document.querySelector(`.s${i}`);
+        thisOne.addEventListener("mouseover", function(){
+            thisOne.classList.add("over");
+        });
+    }
+ 
+}
+
+function reset(){
+
+        container.innerHTML = "";
+        divBox();
+}
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", reset);
+
+submitButton.addEventListener("click", function(){
+let size = document.querySelector(".size");
+squares = Number(size.value) ;
+reset();
+});
+
+divBox();
